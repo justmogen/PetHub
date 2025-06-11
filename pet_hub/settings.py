173d283b0c0a4b132b64ecdaea725fda46ffd47d
black <black@localhost.localdomain>
2025-06-11 22:46:44 +0300

    changing static files settings

--- pet_hub/settings.py
+++ pet_hub/settings.py
@@ -149,6 +149,10 @@ else:
         "default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"},
         "staticfiles": {"BACKEND": "storages.backends.s3boto3.S3StaticStorage"}
     }
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3StaticStorage"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    AWS_S3_SIGNATURE_VERSION = "s3v4"
+    AWS_S3_ADDRESSING_STYLE = "virtual"
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
